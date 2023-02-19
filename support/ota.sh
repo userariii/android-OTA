5467b889e12d335153d4168804553de70e266784
ARINDAM BHATTACHARJEE <abhattacharjee717@gmail.com>
2023-02-19 22:40:18 +0000

    Adapted for pixelexperience

--- support/ota.sh
+++ support/ota.sh
@@ -20,8 +20,8 @@ WEBSITE_URL="https://userariii.github.io/"
 NEWS_URL="https://t.me/SD720G_repo"
 JSON_FMT='{\n\t"error":false,\n\t"filename":"%s",\n\t"datetime":%s,\n\t"size":%s,\n\t"url":"%s",\n\t"filehash":"%s",\n\t"version":"%s",\n\t"status":"%s",\n\t"pexv":"%s",\n\t"id":"%s",\n\t"tg_username":"%s",\n\t"device_name":"%s",\n\t"device":"%s",\n\t"xda_thread":"%s",\n\t"maintainers": [{\n\t\t"main_maintainer":false,\n\t\t"github_username":"%s",\n\t\t"name":"%s"\n\t}],\n\t"donate_url":"%s",\n\t"website_url":"%s",\n\t"news_url":"%s",\n\t"forum_url":"%s"\n}'
 
-printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$STATUS" "$PEXV" "$ID" "$TG_USERNAME" "$DEVICE_NAME" "$DEVICE" "$XDA_THREAD" "$GHUN" "$NAME" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$XDA_THREAD" > OTA/builds/$DEVICE.json
-echo OTA/builds/$DEVICE.json file created
+printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$STATUS" "$PEXV" "$ID" "$TG_USERNAME" "$DEVICE_NAME" "$DEVICE" "$XDA_THREAD" "$GHUN" "$NAME" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$XDA_THREAD" > OTA/pixelexperience/tiramisu/builds/$DEVICE.json
+echo OTA/pixelexperience/tiramisu/builds/$DEVICE.json file created
 
 BUILD_DATE=$(echo $FILENAME | cut -d "-" -f 3)
 BUILD_YEAR=${BUILD_DATE:0:4}
@@ -29,12 +29,12 @@ BUILD_MONTH=${BUILD_DATE:4:2}
 BUILD_DAY=${BUILD_DATE:6:2}
 CHANGELOG=$(echo "Changelog - $BUILD_YEAR/$BUILD_MONTH/$BUILD_DAY\n")
 
-if [ -d "OTA/changelogs/$DEVICE" ]
+if [ -d "OTA/pixelexperience/tiramisu/changelogs/$DEVICE" ]
 then
-printf "$CHANGELOG" > OTA/changelogs/$DEVICE/$FILENAME.txt
-echo OTA/changelogs/$DEVICE/$FILENAME.txt file created "(make sure to update the changelog file)"
+printf "$CHANGELOG" > OTA/pixelexperience/tiramisu/changelogs/$DEVICE/$FILENAME.txt
+echo OTA/pixelexperience/tiramisu/changelogs/$DEVICE/$FILENAME.txt file created "(make sure to update the changelog file)"
 else
-mkdir -p OTA/changelogs/$DEVICE
+mkdir -p OTA/pixelexperience/tiramisu/changelogs/$DEVICE
 printf "$CHANGELOG" > OTA/changelogs/$DEVICE/$FILENAME.txt
 echo OTA/changelogs/$DEVICE/$FILENAME.txt file created "(make sure to update the changelog file)"
 fi
